2a7e825f970f78f53e0e9cd353e5a67c12f44098
piotrkica <piotr.kica.35@gmail.com>
2023-06-08 20:43:50 +0200

    replace apt with apt-get, install requests in ec2_create.sh

--- TranscriptomicsAtlas/EC2/ec2_create.sh
+++ TranscriptomicsAtlas/EC2/ec2_create.sh
@@ -19,17 +19,17 @@ wget https://github.com/COMBINE-lab/salmon/releases/download/v1.10.0/salmon-1.10
 echo 'export PATH="$PATH":/home/ubuntu/salmon-latest_linux_x86_64/bin' >> ~/.bashrc
 
 ### PYTHON MODULES
-sudo apt install python3-pip -y
-pip3 install boto3 watchtower --no-cache-dir
+sudo apt-get install python3-pip -y
+pip3 install boto3 watchtower requests --no-cache-dir
 
 ### R
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/" -y
-sudo apt install --no-install-recommends r-base-dev -y
+sudo apt-get install --no-install-recommends r-base-dev -y
 
 ### R packages
 # 'DESeq2' and 'RCurl' (library required by DESeq2) requires
-sudo apt install libxml2-dev libssl-dev libcurl4-openssl-dev -y
+sudo apt-get install libxml2-dev libssl-dev libcurl4-openssl-dev -y
 # 'RcppArmadillo' (library required by DESeq2) requires
 sudo apt-get install libopenblas-dev -y
 
