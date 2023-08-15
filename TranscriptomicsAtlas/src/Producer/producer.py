d65032847f3e34a064156ac47084239e58b6f332
piotrkica <piotr.kica.35@gmail.com>
2023-08-15 15:17:39 +0200

    producer.py ready with 99 IDs to send

--- TranscriptomicsAtlas/src/Producer/producer.py
+++ TranscriptomicsAtlas/src/Producer/producer.py
@@ -1,6 +1,13 @@
 import boto3
+from tqdm import trange
 
 sqs = boto3.resource("sqs")
-queue = sqs.get_queue_by_name(QueueName="NearData_queue")
+queue = sqs.get_queue_by_name(QueueName="NearData_queue_container")
 
-queue.send_message(MessageBody="SRR11858779")
+with open("../../data/SRA_IDs/random_99_small_SRA_IDs.txt", "r") as f:
+    sra_ids_all = [sra_id.strip() for sra_id in f.readlines()]
+
+for i in trange(0, len(sra_ids_all), 10):
+    sra_ids = sra_ids_all[i:i + 10]
+    entries = [{"Id": sra_id, "MessageBody": sra_id} for sra_id in sra_ids]
+    queue.send_messages(Entries=entries)
