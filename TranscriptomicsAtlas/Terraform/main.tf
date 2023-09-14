c6478f14ede86e7679518fb331d3098cef3024d8
piotrkica <piotr.kica.35@gmail.com>
2023-09-14 14:19:12 +0200

    increase message_retention_seconds in sqs queues

--- TranscriptomicsAtlas/Terraform/main.tf
+++ TranscriptomicsAtlas/Terraform/main.tf
@@ -17,7 +17,7 @@ data "aws_iam_instance_profile" "NearData_ec2_role" {
 resource "aws_sqs_queue" "NearData_queue" {
   name                       = "NearData_queue"
   max_message_size           = 2048
-  message_retention_seconds  = 36000
+  message_retention_seconds  = 604800
   receive_wait_time_seconds  = 5
   visibility_timeout_seconds = 18000
 
@@ -46,7 +46,7 @@ resource "aws_sqs_queue" "NearData_deadletter_queue" {
 resource "aws_sqs_queue" "NearData_queue_container" {
   name                       = "NearData_queue_container"
   max_message_size           = 2048
-  message_retention_seconds  = 36000
+  message_retention_seconds  = 604800
   receive_wait_time_seconds  = 5
   visibility_timeout_seconds = 18000
 
