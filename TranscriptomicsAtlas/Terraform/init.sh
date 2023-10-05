66ab5bd0485c78b8a04c33261a9c7042d5b4ea2a
piotrkica <piotr.kica.35@gmail.com>
2023-10-05 16:05:21 +0200

    add env var start_cwagent

--- TranscriptomicsAtlas/Terraform/init.sh
+++ TranscriptomicsAtlas/Terraform/init.sh
@@ -8,6 +8,7 @@ echo export s3_bucket_name="neardata-salmon-ec2-results"
 echo export s3_bucket_name_low_mr="neardata-salmon-ec2-results-low-mr"
 echo export dynamodb_metadata_table="neardata-test-table"
 echo export execution_mode="EC2"
+echo export start_cwagent="True"
 } >> /etc/environment
 
 su ubuntu -c "python3 /opt/TAtlas/Consumer/consumer.py"
--- TranscriptomicsAtlas/container_start.py
+++ TranscriptomicsAtlas/container_start.py
@@ -1,7 +1,10 @@
+import os
 import subprocess
 
 if __name__=="__main__":
-    print("Starting Agent")
-    cwagent = subprocess.Popen(["/opt/aws/amazon-cloudwatch-agent/bin/start-amazon-cloudwatch-agent"])
+    if os.environ["start_cwagent"] == "True":
+        print("Starting Agent")
+        cwagent = subprocess.Popen(["/opt/aws/amazon-cloudwatch-agent/bin/start-amazon-cloudwatch-agent"])
+
     print("Staring consumer")
     subprocess.run(["python3", "/opt/TAtlas/Consumer/consumer.py"])
