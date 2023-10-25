e79c4c79bd41b5c69852ac14caa978580e152b27
piotrkica <piotr.kica.35@gmail.com>
2023-10-25 15:23:18 +0200

    use os.sched_getaffinity way to check nproc value

--- TranscriptomicsAtlas/src/Consumer/config.py
+++ TranscriptomicsAtlas/src/Consumer/config.py
@@ -1,7 +1,6 @@
 import os
-import subprocess
 
-nproc = subprocess.run(["nproc"], capture_output=True, text=True).stdout.strip()
+nproc = str(len(os.sched_getaffinity(0)))
 my_env = {**os.environ, 'PATH': '/opt/TAtlas/sratoolkit.3.0.1-ubuntu64/bin:'
                                 '/opt/TAtlas/salmon-latest_linux_x86_64/bin:' + os.environ['PATH']}
 work_dir = "/home/ubuntu/TAtlas"
