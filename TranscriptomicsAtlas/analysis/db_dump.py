e07cf7533d4c890670d58e8d7dc75b43046e538a
piotrkica <piotr.kica.35@gmail.com>
2023-12-19 20:08:43 +0100

    small improvements to dynamodb dump table code

--- TranscriptomicsAtlas/analysis/db_dump.py
+++ TranscriptomicsAtlas/analysis/db_dump.py
@@ -7,21 +7,22 @@ def dump_metadata_table(table_name):
     table = dynamodb.Table(table_name)
 
     db = []
-    done = False
     start_key = None
-    while not done:
+    while True:
         if not start_key:
             response = table.scan()
         else:
             response = table.scan(ExclusiveStartKey=start_key)
         db.extend(response.get('Items', []))
         start_key = response.get('LastEvaluatedKey', None)
-        done = start_key is None
+        if start_key is None:
+            break
 
     df = pd.DataFrame(db)
     df = df[["SRR_id",
              "bucket",
              "tissue_name",
+             "error_type",
              "salmon_mapping_rate [%]",
              "SRR_filesize_bytes",
              "fastq_filesize_bytes",
@@ -36,9 +37,9 @@ def dump_metadata_table(table_name):
              "deseq2_start_time",
              "deseq2_end_time"]]
     df = df.sort_values(["tissue_name", "salmon_mapping_rate [%]"], ascending=[True, False]).reset_index(drop=True)
-    df.to_csv("metadata_db_dump.csv")
+    df.to_csv("data/metadata_db_dump_HPC.csv")
 
     return df
 
 
-df = dump_metadata_table("neardata-tissues-salmon-metadata")
+dump_metadata_table("neardata-tissues-salmon-metadata")
