9fec7a67abffc3e90da1fbb788a8a21f2bdda224
piotrkica <piotr.kica.35@gmail.com>
2023-12-19 19:43:06 +0100

    use log_group_name in LogHandler, fixes

--- TranscriptomicsAtlas/src/Consumer/logger.py
+++ TranscriptomicsAtlas/src/Consumer/logger.py
@@ -7,7 +7,9 @@ from utils import PipelineError
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.addHandler(watchtower.CloudWatchLogHandler(send_interval=1, log_stream_name=os.getenv('SLURM_JOB_ID', "")+'/{machine_name}/{program_name}/{logger_name}/{process_id}'))
+log_stream_name = os.getenv('SLURM_JOB_ID', "")+'/{machine_name}/{program_name}/{logger_name}/{process_id}'
+log_handler = watchtower.CloudWatchLogHandler(send_interval=1, log_group_name="STAR-EC2", log_stream_name=log_stream_name)
+logger.addHandler(log_handler)
 
 
 def log_output(func):
@@ -20,7 +22,7 @@ def log_output(func):
         logger.info(result.stdout)
         logger.warning(result.stderr)
         if result.returncode != 0:
-            raise PipelineError(f"{func.__name__} failed. Aborting the pipeline.")
+            raise PipelineError(f"{func.__name__} failed. Aborting the pipeline.", f"{func.__name__} failed")
         logger.info(f"{func.__name__} finished")
 
         return result
