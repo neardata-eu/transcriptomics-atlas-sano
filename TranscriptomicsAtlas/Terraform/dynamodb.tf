be0fdd8659edeabe2d16f2da0224fb580bf76d70
piotrkica <piotr.kica.35@gmail.com>
2024-01-05 14:40:39 +0100

    tf: new queues, buckets, dynamodb tables for star and HPC results

--- TranscriptomicsAtlas/Terraform/dynamodb.tf
+++ TranscriptomicsAtlas/Terraform/dynamodb.tf
@@ -18,6 +18,27 @@ resource "aws_dynamodb_table" "NearData_db" {
   }
 }
 
+resource "aws_dynamodb_table" "NearData_db_hpc" {
+  name                        = "neardata-tissues-salmon-metadata_hpc"
+  billing_mode                = "PAY_PER_REQUEST"
+  hash_key                    = "SRR_id"
+  deletion_protection_enabled = true
+
+  attribute {
+    name = "SRR_id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "NearData_db_hpc"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+
 resource "aws_dynamodb_table" "NearData_test_db" {
   name         = "neardata-test-table"
   billing_mode = "PAY_PER_REQUEST"
@@ -30,5 +51,25 @@ resource "aws_dynamodb_table" "NearData_test_db" {
 
   tags = {
     Name = "NearData_test_db"
+  }
+}
+
+resource "aws_dynamodb_table" "STAR_metadata_db" {
+  name                        = "neardata-tissues-star-metadata"
+  billing_mode                = "PAY_PER_REQUEST"
+  hash_key                    = "SRR_id"
+  deletion_protection_enabled = true
+
+  attribute {
+    name = "SRR_id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "NearData_db"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
--- TranscriptomicsAtlas/Terraform/sqs.tf
+++ TranscriptomicsAtlas/Terraform/sqs.tf
@@ -18,3 +18,45 @@ resource "aws_sqs_queue" "NearData_deadletter_queue" {
   receive_wait_time_seconds  = 5
   visibility_timeout_seconds = 10800
 }
+
+resource "aws_sqs_queue" "NearData_queue_hpc" {
+  name                       = "NearData_queue_hpc"
+  max_message_size           = 2048
+  message_retention_seconds  = 604800
+  receive_wait_time_seconds  = 5
+  visibility_timeout_seconds = 18000
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.NearData_deadletter_queue_hpc.arn
+    maxReceiveCount     = 1
+  })
+}
+
+resource "aws_sqs_queue" "NearData_deadletter_queue_hpc" {
+  name                       = "NearData_deadletter_queue_hpc"
+  max_message_size           = 2048
+  message_retention_seconds  = 604800
+  receive_wait_time_seconds  = 5
+  visibility_timeout_seconds = 10800
+}
+
+resource "aws_sqs_queue" "STAR_queue" {
+  name                       = "STAR_queue"
+  max_message_size           = 2048
+  message_retention_seconds  = 604800
+  receive_wait_time_seconds  = 5
+  visibility_timeout_seconds = 36000
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.STAR_deadletter_queue.arn
+    maxReceiveCount     = 1
+  })
+}
+
+resource "aws_sqs_queue" "STAR_deadletter_queue" {
+  name                       = "STAR_deadletter_queue"
+  max_message_size           = 2048
+  message_retention_seconds  = 604800
+  receive_wait_time_seconds  = 5
+  visibility_timeout_seconds = 36000
+}
